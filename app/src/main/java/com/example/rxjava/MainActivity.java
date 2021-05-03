87060a98242186fed71708008eba8646efb8a4af
Muhammad Nur Yanhaona <nur.yanhaona@konasl.com>
2021-05-03 14:44:20 +0600

    fromPublisher for reactive streams

--- app/src/main/java/com/example/rxjava/MainActivity.java
+++ app/src/main/java/com/example/rxjava/MainActivity.java
@@ -44,41 +44,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         MainViewModel viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
-        try {
-            viewModel.makeFutureQuery().get()
-                    .subscribeOn(Schedulers.io())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(new Observer<ResponseBody>() {
-                        @Override
-                        public void onSubscribe(Disposable d) {
-                            Log.d(TAG, "onSubscribe: called.");
-                        }
-
-                        @Override
-                        public void onNext(ResponseBody responseBody) {
-                            Log.d(TAG, "onNext: got the response from server!");
-                            try {
-                                Log.d(TAG, "onNext: " + responseBody.string());
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-
-                        @Override
-                        public void onError(Throwable e) {
-                            Log.e(TAG, "onError: ", e);
-                        }
-
-                        @Override
-                        public void onComplete() {
-                            Log.d(TAG, "onComplete: called.");
-                        }
-                    });
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        viewModel.makeQuery().observe(this,
+                new androidx.lifecycle.Observer<ResponseBody>() {
+            @Override
+            public void onChanged(ResponseBody responseBody) {
+                Log.d(TAG, "onChanged: this is a live data response");
+                try {
+                    Log.d(TAG, "onChanged: " + responseBody.string());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     private void testTimer() {
